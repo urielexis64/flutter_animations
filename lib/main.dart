acbfd9f74e5a3f7948ab0598df571368e96fa0e1
Uriel Aispuro <urielalexis64@gmail.com>
2021-05-04 11:46:59 -0600

    initial workflow setup

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:custom_painter/src/challenges/animated_square_page.dart';
+import 'package:custom_painter/src/pages/animations_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Designs App',
+        home: AnimatedSquarePage());
+  }
+}

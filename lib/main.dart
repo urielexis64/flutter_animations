6381c6f27969aae6462add564ab773a48801004c
Uriel Aispuro <urielalexis64@gmail.com>
2021-05-15 00:27:29 -0600

    launcher tablet page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:custom_painter/src/pages/launcher_page.dart';
+import 'package:custom_painter/src/pages/launcher_tablet%20page.dart';
 import 'package:custom_painter/src/shared_prefs/user_preferences.dart';
 import 'package:custom_painter/theme/theme.dart';
 import 'package:flutter/material.dart';
@@ -17,9 +18,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: Provider.of<ThemeChanger>(context).currentTheme,
-        debugShowCheckedModeBanner: false,
-        title: 'Designs App',
-        home: LauncherPage());
+      theme: Provider.of<ThemeChanger>(context).currentTheme,
+      debugShowCheckedModeBanner: false,
+      title: 'Designs App',
+      home: OrientationBuilder(
+        builder: (BuildContext context, Orientation orientation) {
+          final screenSize = MediaQuery.of(context).size;
+          if (screenSize.width > 500) {
+            return LauncherTabletPage();
+          }
+          return LauncherPage();
+        },
+      ),
+    );
   }
 }
---
+++ lib/src/pages/launcher_tablet page.dart
@@ -0,0 +1,115 @@
+import 'package:custom_painter/src/pages/slideshow_page.dart';
+import 'package:custom_painter/src/routes/routes.dart';
+import 'package:custom_painter/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+
+class LauncherTabletPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTheme = Provider.of<ThemeChanger>(context);
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter Tablet Designs')),
+      drawer: _MainMenu(),
+      body: Row(
+        children: [
+          Container(
+            width: size.width * .4,
+            height: double.infinity,
+            child: _OptionsList(),
+          ),
+          Container(
+            width: 1,
+            height: double.infinity,
+            color: appTheme.darkTheme
+                ? Colors.grey
+                : appTheme.currentTheme.accentColor,
+          ),
+          Expanded(child: SlideshowPage())
+        ],
+      ),
+    );
+  }
+}
+
+class _OptionsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTheme = Provider.of<ThemeChanger>(context).currentTheme;
+
+    return ListView.separated(
+      physics: BouncingScrollPhysics(),
+      separatorBuilder: (context, i) => Divider(
+        color: appTheme.primaryColorLight,
+      ),
+      itemCount: pageRoutes.length,
+      itemBuilder: (context, index) => ListTile(
+        leading: FaIcon(
+          pageRoutes[index].icon,
+          color: appTheme.accentColor,
+        ),
+        title: Text(pageRoutes[index].title),
+        trailing: Icon(Icons.chevron_right, color: appTheme.accentColor),
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => pageRoutes[index].page,
+              ));
+        },
+      ),
+    );
+  }
+}
+
+class _MainMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTheme = Provider.of<ThemeChanger>(context);
+    final accentColor = appTheme.currentTheme.accentColor;
+
+    return SafeArea(
+      child: Drawer(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                width: double.infinity,
+                height: 200,
+                child: CircleAvatar(
+                  backgroundColor: accentColor,
+                  child: Text(
+                    'UA',
+                    style: TextStyle(
+                      fontSize: 50,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(child: _OptionsList()),
+              ListTile(
+                leading: Icon(Icons.lightbulb_outline, color: accentColor),
+                title: Text('Dark Mode'),
+                trailing: Switch.adaptive(
+                    value: appTheme.darkTheme,
+                    activeColor: accentColor,
+                    onChanged: (value) => appTheme.darkTheme = value),
+              ),
+              ListTile(
+                leading: Icon(Icons.add_to_home_screen, color: accentColor),
+                title: Text('Custom Mode'),
+                trailing: Switch.adaptive(
+                    value: appTheme.customTheme,
+                    activeColor: accentColor,
+                    onChanged: (value) => appTheme.customTheme = value),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
